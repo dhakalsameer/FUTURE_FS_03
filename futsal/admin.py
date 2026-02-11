7e401b9b678404d3306b18e4f24f99840ab1c9a5
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-11 16:22:12 +0545

    modifying tge admin.py

--- futsal/admin.py
+++ futsal/admin.py
@@ -7,8 +7,30 @@ from .models import (
     GalleryImage
 )
 
-admin.site.register(FutsalInfo)
-admin.site.register(Facility)
-admin.site.register(Booking)
-admin.site.register(ContactMessage)
-admin.site.register(GalleryImage)
+
+@admin.register(FutsalInfo)
+class FutsalInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'price_per_hour')
+
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'date', 'time_slot', 'created_at')
+    list_filter = ('date',)
+    search_fields = ('name', 'phone')
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+
+
+@admin.register(GalleryImage)
+class GalleryImageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
