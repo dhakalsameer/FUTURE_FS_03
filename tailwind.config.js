5f7535bc9335c20392ca7573fe382e3dae04d879
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-13 17:30:26 +0545

    chore: Add local Tailwind CSS setup files (currently unused)
    Added local Tailwind CSS setup files, including:
    -  (input CSS for Tailwind)
    -  (npm dependencies)
    -
    -  (with Tailwind build script)
    -
    -
    
    These files were part of an attempt to move Tailwind CSS to a local build
    process. While the local build is currently not active (project is using
    Tailwind CDN), these files are committed as requested by the user.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './futsal/templates/**/*.html',
+    './futsal/static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
